ac3bb9d22f7cbb33da4898210c9068f3d8d80869
osmankizildag <buchhalterkizildag@gmail.com>
2023-01-08 14:05:33 +0100

    080123.1405.osman

---
+++ src/gun08_team4/S01_Team4OrtakSinif.java
@@ -0,0 +1,14 @@
+package gun08_team4;
+
+public class S01_Team4OrtakSinif {
+    public static void main(String[] args) {
+
+        System.out.println("Genel Açıklamalar: Burası ortak sınıfımız, " +
+                "conflict safhasına geçinceye kadar lutfen birer satir boş" +
+                "bırakarak yazalım. asagıdakı ornekte oldugu gibi " +
+                "mesajın sonuna ismimizi de yazalım. " +
+                "Ayrıca kendimize ait ismimizle birer class da açalım");
+
+        System.out.println("merhaba yılmak yok, yola devam. osman");
+    }
+}
---
+++ src/gun08_team4/S02_osmanClass.java
@@ -0,0 +1,8 @@
+package gun08_team4;
+
+public class S02_osmanClass {
+    public static void main(String[] args) {
+
+        System.out.println("merhaba yeni proje merhaba yeni hayat");
+    }
+}
