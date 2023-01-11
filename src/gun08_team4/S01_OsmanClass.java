53cbfa6b0a0a4bbf760c9c2ba0c3b84d714d4930
osman kizildag <116977882+osmankizildag@users.noreply.github.com>
2023-01-11 06:42:12 +0100

    Update S01_OsmanClass.java

--- src/gun08_team4/S01_OsmanClass.java
+++ src/gun08_team4/S01_OsmanClass.java
@@ -5,18 +5,6 @@ public class S01_OsmanClass {
 
         System.out.println("merhaba yeni proje merhaba yeni hayat");
 
-        System.out.println("ılk pulldan sonra gonderdıgım mesaj");
-
-        System.out.println("ucuncu degısıklık mesajım");
-
-        System.out.println("dorduncu mesajım");
-
-        System.out.println("besıncı mesajım");
-
-        System.out.println("altıncı mesajım");
-
-        System.out.println("yedıncı mesajım");
-
-        System.out.println("sekizinci mesajım");
+     
     }
 }
