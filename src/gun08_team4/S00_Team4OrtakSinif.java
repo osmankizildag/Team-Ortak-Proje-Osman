dcb7d0f7881688f0feb500778364302048dca464
osmankizildag <buchhalterkizildag@gmail.com>
2023-01-11 06:37:59 +0100

    110123.0637.osman.conf.1

---
+++ src/gun08_team4/S00_Team4OrtakSinif.java
@@ -0,0 +1,17 @@
+package gun08_team4;
+
+public class S00_Team4OrtakSinif {
+    public static void main(String[] args) {
+
+        System.out.println("Genel Açıklamalar: Burası ortak sınıfımız, " +
+                "conflict safhasına geçinceye kadar lutfen birer satir boş" +
+                "bırakarak yazalım. asagıdakı ornekte oldugu gibi " +
+                "mesajın sonuna ismimizi de yazalım. " +
+                "Ayrıca kendimize ait ismimizle birer class da açalım");
+
+        // 110123.0632.buradaki tum soutları sildim
+        // class ad ve nolarını duzenledım
+        // hedefım conflıt olusturmal, gıthubda duzeltmek
+        //
+    }
+}
--- src/gun08_team4/S01_OsmanClass.java
+++ src/gun08_team4/S01_OsmanClass.java
@@ -1,6 +1,6 @@
 package gun08_team4;
 
-public class S02_osmanClass {
+public class S01_OsmanClass {
     public static void main(String[] args) {
 
         System.out.println("merhaba yeni proje merhaba yeni hayat");
